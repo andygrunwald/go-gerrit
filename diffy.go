02dbb2feeecb0baa147fe689798407ac54b39078
Andy Grunwald <andygrunwald@gmail.com>
2015-09-03 20:05:45 +0200

    Created NewClient and unit tests

--- diffy.go
+++ diffy.go
@@ -1,14 +1,57 @@
 package diffy
 
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
 // A Client manages communication with the Gerrit API.
 type Client struct {
+	// HTTP client used to communicate with the API.
+	client *http.Client
+
+	// Base URL for API requests.
+	// BaseURL should always be specified with a trailing slash.
+	baseURL *url.URL
+
+	// Services used for talking to different parts of the Gerrit API.
+	Access   *AccessService
+	Accounts *AccountsService
+	Changes  *ChangesService
+	Config   *ConfigService
+	Groups   *GroupsService
+	Plugins  *PluginsService
+	Projects *ProjectsService
 }
 
 // NewClient returns a new Gerrit API client.
-func NewClient() *Client {
-	// TODO Use http client?
-	// Like https://github.com/google/go-github/blob/master/github/github.go#L128
-	instance := &Client{}
+// gerritInstance has to be the HTTP endpoint of the Gerrit instance.
+// If a nil httpClient is provided, http.DefaultClient will be used.
+func NewClient(gerritInstance string, httpClient *http.Client) (*Client, error) {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
 
-	return instance
+	if len(gerritInstance) == 0 {
+		return nil, fmt.Errorf("No Gerrit instance given.")
+	}
+	baseURL, err := url.Parse(gerritInstance)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Client{
+		client:  httpClient,
+		baseURL: baseURL,
+	}
+	c.Access = &AccessService{client: c}
+	c.Accounts = &AccountsService{client: c}
+	c.Changes = &ChangesService{client: c}
+	c.Config = &ConfigService{client: c}
+	c.Groups = &GroupsService{client: c}
+	c.Plugins = &PluginsService{client: c}
+	c.Projects = &ProjectsService{client: c}
+
+	return c, nil
 }
--- diffy_test.go
+++ diffy_test.go
@@ -1,13 +1,70 @@
 package diffy
 
 import (
+	"net/http"
+	"reflect"
 	"testing"
+	"time"
 )
 
-func TestNewClient(t *testing.T) {
-	c := NewClient()
+func TestNewClient_NoGerritInstance(t *testing.T) {
+	c, err := NewClient("", nil)
+	if c != nil {
+		t.Errorf("NewClient return is not nil. Expected no client. Go %+v", c)
+	}
+	if err == nil {
+		t.Error("No error occured by empty Gerrit Instance. Expected one.")
+	}
+}
 
-	if c == nil {
-		t.Errorf("NewClient return is nil, but expected a diffy.Client struct")
+func TestNewClient_HttpClient(t *testing.T) {
+	customHTTPClient := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+	mockData := []struct {
+		HTTPClient         *http.Client
+		ExpectedHTTPClient *http.Client
+	}{
+		{nil, http.DefaultClient},
+		{customHTTPClient, customHTTPClient},
+	}
+
+	for _, mock := range mockData {
+		c, err := NewClient("https://gerrit-review.googlesource.com/", mock.HTTPClient)
+		if err != nil {
+			t.Errorf("An error occured. Expected nil. Got %+v.", err)
+		}
+		if reflect.DeepEqual(c.client, mock.ExpectedHTTPClient) == false {
+			t.Errorf("Wrong HTTP Client. Expected %+v. Got %+v", mock.ExpectedHTTPClient, c.client)
+		}
+	}
+}
+
+func TestNewClient_Services(t *testing.T) {
+	c, err := NewClient("https://gerrit-review.googlesource.com/", nil)
+	if err != nil {
+		t.Errorf("An error occured. Expected nil. Got %+v.", err)
+	}
+
+	if c.Access == nil {
+		t.Error("No AccessService found.")
+	}
+	if c.Accounts == nil {
+		t.Error("No AccountsService found.")
+	}
+	if c.Changes == nil {
+		t.Error("No ChangesService found.")
+	}
+	if c.Config == nil {
+		t.Error("No ConfigService found.")
+	}
+	if c.Groups == nil {
+		t.Error("No GroupsService found.")
+	}
+	if c.Plugins == nil {
+		t.Error("No PluginsService found.")
+	}
+	if c.Projects == nil {
+		t.Error("No ProjectsService found.")
 	}
 }
