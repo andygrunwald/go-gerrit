d581a778a11b671aa45d4e2dfd893a868109442f
Andy Grunwald <andygrunwald@gmail.com>
2015-09-05 15:05:37 +0200

    Add basic struct structure for plugins

--- plugins.go
+++ plugins.go
@@ -6,3 +6,16 @@ package diffy
 type PluginsService struct {
 	client *Client
 }
+
+// PluginInfo entity describes a plugin.
+type PluginInfo struct {
+	ID       string `json:"id"`
+	Version  string `json:"version"`
+	IndexURL string `json:"index_url,omitempty"`
+	Disabled bool   `json:"disabled,omitempty"`
+}
+
+// PluginInput entity describes a plugin that should be installed.
+type PluginInput struct {
+	URL string `json:"url"`
+}
