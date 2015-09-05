af613d76a2859af32cfe1ce09cd3f1e19b146b86
Andy Grunwald <andygrunwald@gmail.com>
2015-09-05 12:43:38 +0200

    Set omitempty flags for change structures

--- changes.go
+++ changes.go
@@ -27,22 +27,22 @@ type GitPersonInfo struct {
 // ActionInfo entity describes a REST API call the client can make to manipulate a resource.
 // These are frequently implemented by plugins and may be discovered at runtime.
 type ActionInfo struct {
-	Method  string `json:"method"`
-	Label   string `json:"label"`
-	Title   string `json:"title"`
-	Enabled bool   `json:"enabled"`
+	Method  string `json:"method,omitempty"`
+	Label   string `json:"label,omitempty"`
+	Title   string `json:"title,omitempty"`
+	Enabled bool   `json:"enabled,omitempty"`
 }
 
 // AbandonInput entity contains information for abandoning a change.
 type AbandonInput struct {
-	Message string `json:"message"`
+	Message string `json:"message,omitempty"`
 }
 
 // AddReviewerResult entity describes the result of adding a reviewer to a change.
 type AddReviewerResult struct {
-	Reviewers []ReviewerInfo `json:"reviewers"`
-	Error     string         `json:"error"`
-	Confirm   bool           `json:"confirm"`
+	Reviewers []ReviewerInfo `json:"reviewers,omitempty"`
+	Error     string         `json:"error,omitempty"`
+	Confirm   bool           `json:"confirm,omitempty"`
 }
 
 // ReviewerInfo entity contains information about a reviewer and its votes on a change.
@@ -54,15 +54,15 @@ type ReviewerInfo struct {
 // ApprovalInfo entity contains information about an approval from a user for a label on a change.
 type ApprovalInfo struct {
 	AccountInfo
-	Value int    `json:"value"`
-	Date  string `json:"date"`
+	Value int    `json:"value,omitempty"`
+	Date  string `json:"date,omitempty"`
 }
 
 // ChangeEditInput entity contains information for restoring a path within change edit.
 type ChangeEditInput struct {
-	RestorePath string `json:"restore_path"`
-	OldPath     string `json:"old_path"`
-	NewPath     string `json:"new_path"`
+	RestorePath string `json:"restore_path,omitempty"`
+	OldPath     string `json:"old_path,omitempty"`
+	NewPath     string `json:"new_path,omitempty"`
 }
 
 // ChangeEditMessageInput entity contains information for changing the commit message within a change edit.
@@ -73,10 +73,10 @@ type ChangeEditMessageInput struct {
 // ChangeMessageInfo entity contains information about a message attached to a change.
 type ChangeMessageInfo struct {
 	ID             string      `json:"id"`
-	Author         AccountInfo `json:"author"`
+	Author         AccountInfo `json:"author,omitempty"`
 	Date           string      `json:"date"`
 	Message        string      `json:"message"`
-	RevisionNumber int         `json:"_revision_number"`
+	RevisionNumber int         `json:"_revision_number,omitempty"`
 }
 
 // CherryPickInput entity contains information for cherry-picking a change to a new branch.
@@ -98,7 +98,7 @@ type DiffFileMetaInfo struct {
 	Name        string        `json:"name"`
 	ContentType string        `json:"content_type"`
 	Lines       int           `json:"lines"`
-	WebLinks    []WebLinkInfo `json:"web_links"`
+	WebLinks    []WebLinkInfo `json:"web_links,omitempty"`
 }
 
 // DiffWebLinkInfo entity describes a link on a diff screen to an external site.
@@ -112,7 +112,7 @@ type DiffWebLinkInfo struct {
 
 // EditFileInfo entity contains additional information of a file within a change edit.
 type EditFileInfo struct {
-	WebLinks []WebLinkInfo `json:"web_links"`
+	WebLinks []WebLinkInfo `json:"web_links,omitempty"`
 }
 
 // EditInfo entity contains information about a change edit.
@@ -120,23 +120,23 @@ type EditInfo struct {
 	Commit       CommitInfo           `json:"commit"`
 	BaseRevision string               `json:"baseRevision"`
 	Fetch        map[string]FetchInfo `json:"fetch"`
-	Files        map[string]FileInfo  `json:"files"`
+	Files        map[string]FileInfo  `json:"files,omitempty"`
 }
 
 // FetchInfo entity contains information about how to fetch a patch set via a certain protocol.
 type FetchInfo struct {
 	URL      string            `json:"url"`
 	Ref      string            `json:"ref"`
-	Commands map[string]string `json:"commands"`
+	Commands map[string]string `json:"commands,omitempty"`
 }
 
 // FileInfo entity contains information about a file in a patch set.
 type FileInfo struct {
-	Status        string `json:"status"`
-	Binary        bool   `json:"binary"`
-	OldPath       string `json:"old_path"`
-	LinesInserted int    `json:"lines_inserted"`
-	LinesDeleted  int    `json:"lines_deleted"`
+	Status        string `json:"status,omitempty"`
+	Binary        bool   `json:"binary,omitempty"`
+	OldPath       string `json:"old_path,omitempty"`
+	LinesInserted int    `json:"lines_inserted,omitempty"`
+	LinesDeleted  int    `json:"lines_deleted,omitempty"`
 }
 
 // FixInput entity contains options for fixing commits using the fix change endpoint.
@@ -155,27 +155,27 @@ type GroupBaseInfo struct {
 type IncludedInInfo struct {
 	Branches []string          `json:"branches"`
 	Tags     []string          `json:"tags"`
-	External map[string]string `json:"external"`
+	External map[string]string `json:"external,omitempty"`
 }
 
 // MergeableInfo entity contains information about the mergeability of a change.
 type MergeableInfo struct {
 	SubmitType    string   `json:"submit_type"`
 	Mergeable     bool     `json:"mergeable"`
-	MergeableInto []string `json:"mergeable_into"`
+	MergeableInto []string `json:"mergeable_into,omitempty"`
 }
 
 // ProblemInfo entity contains a description of a potential consistency problem with a change.
 // These are not related to the code review process, but rather indicate some inconsistency in Gerrit’s database or repository metadata related to the enclosing change.
 type ProblemInfo struct {
 	Message string `json:"message"`
-	Status  string `json:"status"`
-	Outcome string `json:"outcome"`
+	Status  string `json:"status,omitempty"`
+	Outcome string `json:"outcome,omitempty"`
 }
 
 // RebaseInput entity contains information for changing parent when rebasing.
 type RebaseInput struct {
-	Base string `json:"base"`
+	Base string `json:"base,omitempty"`
 }
 
 // RelatedChangesInfo entity contains information about related changes.
@@ -185,12 +185,12 @@ type RelatedChangesInfo struct {
 
 // RestoreInput entity contains information for restoring a change.
 type RestoreInput struct {
-	Message string `json:"message"`
+	Message string `json:"message,omitempty"`
 }
 
 // RevertInput entity contains information for reverting a change.
 type RevertInput struct {
-	Message string `json:"message"`
+	Message string `json:"message,omitempty"`
 }
 
 // ReviewInfo entity contains information about a review.
@@ -200,18 +200,18 @@ type ReviewInfo struct {
 
 // TopicInput entity contains information for setting a topic.
 type TopicInput struct {
-	Topic string `json:"topic"`
+	Topic string `json:"topic,omitempty"`
 }
 
 // SubmitRecord entity describes results from a submit_rule.
 type SubmitRecord struct {
 	Status       string                            `json:"status"`
-	Ok           map[string]map[string]AccountInfo `json:"ok"`
-	Reject       map[string]map[string]AccountInfo `json:"reject"`
-	Need         map[string]interface{}            `json:"need"`
-	May          map[string]map[string]AccountInfo `json:"may"`
-	Impossible   map[string]interface{}            `json:"impossible"`
-	ErrorMessage string                            `json:"error_message"`
+	Ok           map[string]map[string]AccountInfo `json:"ok,omitempty"`
+	Reject       map[string]map[string]AccountInfo `json:"reject,omitempty"`
+	Need         map[string]interface{}            `json:"need,omitempty"`
+	May          map[string]map[string]AccountInfo `json:"may,omitempty"`
+	Impossible   map[string]interface{}            `json:"impossible,omitempty"`
+	ErrorMessage string                            `json:"error_message,omitempty"`
 }
 
 // SubmitInput entity contains information for submitting a change.
@@ -222,74 +222,74 @@ type SubmitInput struct {
 // SubmitInfo entity contains information about the change status after submitting.
 type SubmitInfo struct {
 	Status     string `json:"status"`
-	OnBehalfOf string `json:"on_behalf_of"`
+	OnBehalfOf string `json:"on_behalf_of,omitempty"`
 }
 
 // RuleInput entity contains information to test a Prolog rule.
 type RuleInput struct {
 	Rule    string `json:"rule"`
-	Filters string `json:"filters"`
+	Filters string `json:"filters,omitempty"`
 }
 
 // ReviewerInput entity contains information for adding a reviewer to a change.
 type ReviewerInput struct {
 	Reviewer  string `json:"reviewer"`
-	Confirmed bool   `json:"confirmed"`
+	Confirmed bool   `json:"confirmed,omitempty"`
 }
 
 // ReviewInput entity contains information for adding a review to a revision.
 type ReviewInput struct {
-	Message      string                    `json:"message"`
-	Labels       map[string]string         `json:"labels"`
-	Comments     map[string][]CommentInput `json:"comments"`
-	StrictLabels bool                      `json:"strict_labels"`
-	Drafts       string                    `json:"drafts"`
-	Notify       string                    `json:"notify"`
-	OnBehalfOf   string                    `json:"on_behalf_of"`
+	Message      string                    `json:"message,omitempty"`
+	Labels       map[string]string         `json:"labels,omitempty"`
+	Comments     map[string][]CommentInput `json:"comments,omitempty"`
+	StrictLabels bool                      `json:"strict_labels,omitempty"`
+	Drafts       string                    `json:"drafts,omitempty"`
+	Notify       string                    `json:"notify,omitempty"`
+	OnBehalfOf   string                    `json:"on_behalf_of,omitempty"`
 }
 
 // RelatedChangeAndCommitInfo entity contains information about a related change and commit.
 type RelatedChangeAndCommitInfo struct {
-	ChangeID              string     `json:"change_id"`
+	ChangeID              string     `json:"change_id,omitempty"`
 	Commit                CommitInfo `json:"commit"`
-	ChangeNumber          int        `json:"_change_number"`
-	RevisionNumber        int        `json:"_revision_number"`
-	CurrentRevisionNumber int        `json:"_current_revision_number"`
-	Status                string     `json:"status"`
+	ChangeNumber          int        `json:"_change_number,omitempty"`
+	RevisionNumber        int        `json:"_revision_number,omitempty"`
+	CurrentRevisionNumber int        `json:"_current_revision_number,omitempty"`
+	Status                string     `json:"status,omitempty"`
 }
 
 // DiffContent entity contains information about the content differences in a file.
 type DiffContent struct {
-	A      string            `json:"a"`
-	B      string            `json:"b"`
-	AB     string            `json:"ab"`
+	A      string            `json:"a,omitempty"`
+	B      string            `json:"b,omitempty"`
+	AB     string            `json:"ab,omitempty"`
 	EditA  DiffIntralineInfo `json:"edit_a,omitempty"`
 	EditB  DiffIntralineInfo `json:"edit_b,omitempty"`
-	Skip   int               `json:"skip"`
-	Common bool              `json:"common"`
+	Skip   int               `json:"skip,omitempty"`
+	Common bool              `json:"common,omitempty"`
 }
 
 // CommitInfo entity contains information about a commit.
 type CommitInfo struct {
-	Commit    string        `json:"commit"`
+	Commit    string        `json:"commit,omitempty"`
 	Parents   []CommitInfo  `json:"parents"`
 	Author    GitPersonInfo `json:"author"`
 	Committer GitPersonInfo `json:"committer"`
 	Subject   string        `json:"subject"`
 	Message   string        `json:"message"`
-	WebLinks  []WebLinkInfo `json:"web_links"`
+	WebLinks  []WebLinkInfo `json:"web_links,omitempty"`
 }
 
 // CommentInput entity contains information for creating an inline comment.
 type CommentInput struct {
-	ID        string       `json:"id"`
-	Path      string       `json:"path"`
-	Side      string       `json:"side"`
-	Line      int          `json:"line"`
-	Range     CommentRange `json:"range"`
-	InReplyTo string       `json:"in_reply_to"`
-	Updated   string       `json:"updated"`
-	Message   string       `json:"message"`
+	ID        string       `json:"id,omitempty"`
+	Path      string       `json:"path,omitempty"`
+	Side      string       `json:"side,omitempty"`
+	Line      int          `json:"line,omitempty"`
+	Range     CommentRange `json:"range,omitempty"`
+	InReplyTo string       `json:"in_reply_to,omitempty"`
+	Updated   string       `json:"updated,omitempty"`
+	Message   string       `json:"message,omitempty"`
 }
 
 // DiffIntralineInfo entity contains information about intraline edits in a file.
@@ -303,88 +303,88 @@ type ChangeInfo struct {
 	ID                 string                  `json:"id"`
 	Project            string                  `json:"project"`
 	Branch             string                  `json:"branch"`
-	Topic              string                  `json:"topic"`
+	Topic              string                  `json:"topic,omitempty"`
 	ChangeID           string                  `json:"change_id"`
 	Subject            string                  `json:"subject"`
 	Status             string                  `json:"status"`
 	Created            string                  `json:"created"`
 	Updated            string                  `json:"updated"`
-	Starred            bool                    `json:"starred"`
-	Reviewed           bool                    `json:"reviewed"`
-	Mergeable          bool                    `json:"mergeable"`
+	Starred            bool                    `json:"starred,omitempty"`
+	Reviewed           bool                    `json:"reviewed,omitempty"`
+	Mergeable          bool                    `json:"mergeable,omitempty"`
 	Insertions         int                     `json:"insertions"`
 	Deletions          int                     `json:"deletions"`
 	Number             int                     `json:"_number"`
 	Owner              AccountInfo             `json:"owner"`
-	Actions            map[string]ActionInfo   `json:"actions"`
-	Labels             map[string]LabelInfo    `json:"labels"`
-	PermittedLabels    map[string][]string     `json:"permitted_labels"`
-	RemovableReviewers []AccountInfo           `json:"removable_reviewers"`
-	Messages           []ChangeMessageInfo     `json:"messages"`
-	CurrentRevision    string                  `json:"current_revision"`
-	Revisions          map[string]RevisionInfo `json:"revisions"`
-	MoreChanges        bool                    `json:"_more_changes"`
-	Problems           []ProblemInfo           `json:"problems"`
-	BaseChange         string                  `json:"base_change"`
+	Actions            map[string]ActionInfo   `json:"actions,omitempty"`
+	Labels             map[string]LabelInfo    `json:"labels,omitempty"`
+	PermittedLabels    map[string][]string     `json:"permitted_labels,omitempty"`
+	RemovableReviewers []AccountInfo           `json:"removable_reviewers,omitempty"`
+	Messages           []ChangeMessageInfo     `json:"messages,omitempty"`
+	CurrentRevision    string                  `json:"current_revision,omitempty"`
+	Revisions          map[string]RevisionInfo `json:"revisions,omitempty"`
+	MoreChanges        bool                    `json:"_more_changes,omitempty"`
+	Problems           []ProblemInfo           `json:"problems,omitempty"`
+	BaseChange         string                  `json:"base_change,omitempty"`
 }
 
 // LabelInfo entity contains information about a label on a change, always corresponding to the current patch set.
 type LabelInfo struct {
-	Optional bool `json:"optional"`
+	Optional bool `json:"optional,omitempty"`
 
 	// Fields set by LABELS
-	Approved     AccountInfo `json:"approved"`
-	Rejected     AccountInfo `json:"rejected"`
-	Recommended  AccountInfo `json:"recommended"`
-	Disliked     AccountInfo `json:"disliked"`
-	Blocking     bool        `json:"blocking"`
-	Value        string      `json:"value"`
-	DefaultValue string      `json:"default_value"`
+	Approved     AccountInfo `json:"approved,omitempty"`
+	Rejected     AccountInfo `json:"rejected,omitempty"`
+	Recommended  AccountInfo `json:"recommended,omitempty"`
+	Disliked     AccountInfo `json:"disliked,omitempty"`
+	Blocking     bool        `json:"blocking,omitempty"`
+	Value        string      `json:"value,omitempty"`
+	DefaultValue string      `json:"default_value,omitempty"`
 
 	// Fields set by DETAILED_LABELS
-	All    []ApprovalInfo    `json:"all"`
-	Values map[string]string `json:"values"`
+	All    []ApprovalInfo    `json:"all,omitempty"`
+	Values map[string]string `json:"values,omitempty"`
 }
 
 // RevisionInfo entity contains information about a patch set.
 type RevisionInfo struct {
-	Draft             bool                  `json:"draft"`
+	Draft             bool                  `json:"draft,omitempty"`
 	Number            int                   `json:"_number"`
 	Created           string                `json:"created"`
 	Uploader          AccountInfo           `json:"uploader"`
 	Ref               string                `json:"ref"`
 	Fetch             map[string]FetchInfo  `json:"fetch"`
-	Commit            CommitInfo            `json:"commit"`
-	Files             map[string]FileInfo   `json:"files"`
-	Actions           map[string]ActionInfo `json:"actions"`
-	Reviewed          bool                  `json:"reviewed"`
-	MessageWithFooter string                `json:"messageWithFooter"`
+	Commit            CommitInfo            `json:"commit,omitempty"`
+	Files             map[string]FileInfo   `json:"files,omitempty"`
+	Actions           map[string]ActionInfo `json:"actions,omitempty"`
+	Reviewed          bool                  `json:"reviewed,omitempty"`
+	MessageWithFooter string                `json:"messageWithFooter,omitempty"`
 }
 
 // CommentInfo entity contains information about an inline comment.
 type CommentInfo struct {
-	PatchSet  int          `json:"patch_set"`
+	PatchSet  int          `json:"patch_set,omitempty"`
 	ID        string       `json:"id"`
-	Path      string       `json:"path"`
-	Side      string       `json:"side"`
-	Line      int          `json:"line"`
-	Range     CommentRange `json:"range"`
-	InReplyTo string       `json:"in_reply_to"`
-	Message   string       `json:"message"`
+	Path      string       `json:"path,omitempty"`
+	Side      string       `json:"side,omitempty"`
+	Line      int          `json:"line,omitempty"`
+	Range     CommentRange `json:"range,omitempty"`
+	InReplyTo string       `json:"in_reply_to,omitempty"`
+	Message   string       `json:"message,omitempty"`
 	Updated   string       `json:"updated"`
-	Author    AccountInfo  `json:"author"`
+	Author    AccountInfo  `json:"author,omitempty"`
 }
 
 // DiffInfo entity contains information about the diff of a file in a revision.
 type DiffInfo struct {
-	MetaA           DiffFileMetaInfo  `json:"meta_a"`
-	MetaB           DiffFileMetaInfo  `json:"meta_b"`
+	MetaA           DiffFileMetaInfo  `json:"meta_a,omitempty"`
+	MetaB           DiffFileMetaInfo  `json:"meta_b,omitempty"`
 	ChangeType      string            `json:"change_type"`
-	IntralineStatus string            `json:"intraline_status"`
+	IntralineStatus string            `json:"intraline_status,omitempty"`
 	DiffHeader      []string          `json:"diff_header"`
 	Content         []DiffContent     `json:"content"`
-	WebLinks        []DiffWebLinkInfo `json:"web_links"`
-	Binary          bool              `json:"binary"`
+	WebLinks        []DiffWebLinkInfo `json:"web_links,omitempty"`
+	Binary          bool              `json:"binary,omitempty"`
 }
 
 // SuggestedReviewerInfo entity contains information about a reviewer that can be added to a change (an account or a group).
