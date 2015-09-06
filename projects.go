3d61294e92e057eae61c687efd0842051e013af9
Andy Grunwald <andygrunwald@gmail.com>
2015-09-06 13:06:13 +0200

    Movec child project functionality to projects_childproject.go

--- projects.go
+++ projects.go
@@ -196,15 +196,6 @@ type ProjectOptions struct {
 	Type string `url:"type,omitempty"`
 }
 
-// ChildProjectOptions specifies the parameters to the Child Project API endpoints.
-//
-// Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-projects.html#list-child-projects
-type ChildProjectOptions struct {
-	// Recursive resolve the child projects of a project recursively.
-	// Child projects that are not visible to the calling user are ignored and are not resolved further.
-	Recursive int `url:"recursive,omitempty"`
-}
-
 // ListProjects lists the projects accessible by the caller.
 // This is the same as using the ls-projects command over SSH, and accepts the same options as query parameters.
 // The entries in the map are sorted by project name.
@@ -331,57 +322,6 @@ func (s *ProjectsService) GetConfig(projectName string) (*ConfigInfo, *Response,
 	}
 
 	v := new(ConfigInfo)
-	resp, err := s.client.Do(req, v)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	return v, resp, err
-}
-
-// ListChildProjects lists the direct child projects of a project.
-//
-// Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-projects.html#list-child-projects
-func (s *ProjectsService) ListChildProjects(projectName string, opt *ChildProjectOptions) (*[]ProjectInfo, *Response, error) {
-	u := fmt.Sprintf("projects/%s/children/", projectName)
-
-	u, err := addOptions(u, opt)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	req, err := s.client.NewRequest("GET", u, nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	v := new([]ProjectInfo)
-	resp, err := s.client.Do(req, v)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	return v, resp, err
-}
-
-// GetChildProject retrieves a child project.
-// If a non-direct child project should be retrieved the parameter recursive must be set.
-//
-// Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-projects.html#get-child-project
-func (s *ProjectsService) GetChildProject(projectName, childProjectName string, opt *ChildProjectOptions) (*ProjectInfo, *Response, error) {
-	u := fmt.Sprintf("projects/%s/children/%s", projectName, childProjectName)
-
-	u, err := addOptions(u, opt)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	req, err := s.client.NewRequest("GET", u, nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	v := new(ProjectInfo)
 	resp, err := s.client.Do(req, v)
 	if err != nil {
 		return nil, resp, err
