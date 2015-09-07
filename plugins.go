136d170a91a49a8b57a184b1149d4f43f6bad549
Andy Grunwald <andygrunwald@gmail.com>
2015-09-07 16:41:58 +0200

    Implemented missing Plugin endpoints

--- plugins.go
+++ plugins.go
@@ -66,8 +66,57 @@ func (s *PluginsService) ListPlugins(opt *PluginOptions) (*map[string]PluginInfo
 // Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-plugins.html#get-plugin-status
 func (s *PluginsService) GetPluginStatus(pluginID string) (*PluginInfo, *Response, error) {
 	u := fmt.Sprintf("plugins/%s/gerrit~status", pluginID)
+	return s.requestWithPluginInfoResponse("GET", u, nil)
+}
 
-	req, err := s.client.NewRequest("GET", u, nil)
+// InstallPlugin installs a new plugin on the Gerrit server.
+// If a plugin with the specified name already exists it is overwritten.
+//
+// Note: if the plugin provides its own name in the MANIFEST file, then the plugin name from the MANIFEST file has precedence over the {plugin-id} above.
+//
+// The plugin jar can either be sent as binary data in the request body or a URL to the plugin jar must be provided in the request body inside a PluginInput entity.
+//
+// As response a PluginInfo entity is returned that describes the plugin.
+// If an existing plugin was overwritten the response is “200 OK”.
+//
+// Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-projects.html#set-dashboard
+func (s *PluginsService) InstallPlugin(pluginID string, input *PluginInput) (*PluginInfo, *Response, error) {
+	u := fmt.Sprintf("plugins/%s", pluginID)
+	return s.requestWithPluginInfoResponse("PUT", u, input)
+}
+
+// EnablePlugin enables a plugin on the Gerrit server.
+//
+// As response a PluginInfo entity is returned that describes the plugin.
+//
+// Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-plugins.html#enable-plugin
+func (s *PluginsService) EnablePlugin(pluginID string) (*PluginInfo, *Response, error) {
+	u := fmt.Sprintf("plugins/%s/gerrit~enable", pluginID)
+	return s.requestWithPluginInfoResponse("POST", u, nil)
+}
+
+// DisablePlugin disables a plugin on the Gerrit server.
+//
+// As response a PluginInfo entity is returned that describes the plugin.
+//
+// Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-plugins.html#disable-plugin
+func (s *PluginsService) DisablePlugin(pluginID string) (*PluginInfo, *Response, error) {
+	u := fmt.Sprintf("plugins/%s/gerrit~disable", pluginID)
+	return s.requestWithPluginInfoResponse("POST", u, nil)
+}
+
+// ReloadPlugin reloads a plugin on the Gerrit server.
+//
+// As response a PluginInfo entity is returned that describes the plugin.
+//
+// Gerrit API docs: https://gerrit-review.googlesource.com/Documentation/rest-api-plugins.html#disable-plugin
+func (s *PluginsService) ReloadPlugin(pluginID string) (*PluginInfo, *Response, error) {
+	u := fmt.Sprintf("plugins/%s/gerrit~reload", pluginID)
+	return s.requestWithPluginInfoResponse("POST", u, nil)
+}
+
+func (s *PluginsService) requestWithPluginInfoResponse(method, u string, input interface{}) (*PluginInfo, *Response, error) {
+	req, err := s.client.NewRequest(method, u, input)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -80,11 +129,3 @@ func (s *PluginsService) GetPluginStatus(pluginID string) (*PluginInfo, *Respons
 
 	return v, resp, err
 }
-
-/*
-Missing Plugin Endpoints
-	Install Plugin
-	Enable Plugin
-	Disable Plugin
-	Reload Plugin
-*/
