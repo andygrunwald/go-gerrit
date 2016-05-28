506ed6f9a559dbdeaed24ea00f7c224c92df9483
Andy Grunwald <andygrunwald@gmail.com>
2016-05-28 22:02:35 +0200

    Fixed "Line 160: error: arg inBody for printf verb %q of wrong type: *gerrit.PermissionRuleInfo (vet)"

--- gerrit_test.go
+++ gerrit_test.go
@@ -157,7 +157,7 @@ func TestNewRequest(t *testing.T) {
 	// Test that body was JSON encoded
 	body, _ := ioutil.ReadAll(req.Body)
 	if got, want := string(body), outBody; got != want {
-		t.Errorf("NewRequest(%q) Body is %v, want %v", inBody, got, want)
+		t.Errorf("NewRequest Body is %v, want %v", got, want)
 	}
 }
 
