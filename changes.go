065556691e4c8ef352a322b43bc922cd80f3e72a
egorse <egorovhome@gmail.com>
2018-03-10 09:24:04 +0200

    Added comment to DiffIntralineInfo from gerrit rest api docs (as suggested by @shurcooL).
    Fixed CommentInput to have Range optional as per gerrit rest api docs. Prior that the generated json had range as empty object and that cause the gerrit return 400 Bad Request.

--- changes.go
+++ changes.go
@@ -240,17 +240,27 @@ type DiffContent struct {
 
 // CommentInput entity contains information for creating an inline comment.
 type CommentInput struct {
-	ID        string       `json:"id,omitempty"`
-	Path      string       `json:"path,omitempty"`
-	Side      string       `json:"side,omitempty"`
-	Line      int          `json:"line,omitempty"`
-	Range     CommentRange `json:"range,omitempty"`
-	InReplyTo string       `json:"in_reply_to,omitempty"`
-	Updated   string       `json:"updated,omitempty"`
-	Message   string       `json:"message,omitempty"`
+	ID        string        `json:"id,omitempty"`
+	Path      string        `json:"path,omitempty"`
+	Side      string        `json:"side,omitempty"`
+	Line      int           `json:"line,omitempty"`
+	Range     *CommentRange `json:"range,omitempty"`
+	InReplyTo string        `json:"in_reply_to,omitempty"`
+	Updated   string        `json:"updated,omitempty"`
+	Message   string        `json:"message,omitempty"`
 }
 
 // DiffIntralineInfo entity contains information about intraline edits in a file.
+//
+// The information consists of a list of <skip length, mark length> pairs,
+// where the skip length is the number of characters between the end of
+// the previous edit and the start of this edit, and the mark length is the
+// number of edited characters following the skip. The start of the edits
+// is from the beginning of the related diff content lines.
+//
+// Note that the implied newline character at the end of each line
+// is included in the length calculation, and thus it is possible for
+// the edits to span newlines.
 type DiffIntralineInfo [][2]int
 
 // ChangeInfo entity contains information about a change.
