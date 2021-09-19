5a2c9c25fb1670f13f245034e52efdf43844f54e
Andy Grunwald <andygrunwald@gmail.com>
2021-09-19 11:21:31 +0200

    New example: Creating a project

---
+++ examples/create_new_project/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/andygrunwald/go-gerrit"
+)
+
+func main() {
+	instance := fmt.Sprintf("http://%s:8080", "localhost")
+	client, err := gerrit.NewClient(instance, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get your credentials
+	// For local development setups, go to
+	// http://localhost:8080/settings/#HTTPCredentials
+	// and click `GENERATE NEW PASSWORD`.
+	// Replace `secret` with your new value.
+	client.Authentication.SetBasicAuth("admin", "secret")
+
+	gerritProject := "Example project to the moon"
+	gerritBranch := "main"
+
+	data := &gerrit.ProjectInput{
+		Name:              gerritProject,
+		Branches:          []string{gerritBranch},
+		CreateEmptyCommit: true,
+	}
+	projectInfo, _, err := client.Projects.CreateProject(gerritProject, data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Project '%s' created with ID '%+v'", projectInfo.Name, projectInfo.ID)
+
+	// Project 'Example project to the moon' created with ID 'Example+project+to+the+moon'
+}
