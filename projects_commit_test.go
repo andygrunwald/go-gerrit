f645b08d7617256516efa7feb52bbd2affe65e8a
swift <35773579+Seizens@users.noreply.github.com>
2022-09-07 03:22:26 +0800

    add: project commits included in (#122)

---
+++ projects_commit_test.go
@@ -0,0 +1,39 @@
+package gerrit_test
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/andygrunwald/go-gerrit"
+)
+
+// +func (s *ProjectsService) GetIncludeIn(projectName, commitID string) (*IncludedInInfo, *Response, error){
+func TestProjectsService_GetIncludeIn(t *testing.T) {
+	setup()
+	defer teardown()
+
+	testMux.HandleFunc("/projects/swift/commits/a8a477efffbbf3b44169bb9a1d3a334cbbd9aa96/in", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `)]}'`+"\n"+`{"branches": ["master"],"tags": ["1.1.0"]}`)
+	})
+
+	includedInInfo, _, err := testClient.Projects.GetIncludeIn("swift", "a8a477efffbbf3b44169bb9a1d3a334cbbd9aa96")
+	if err != nil {
+		t.Errorf("Projects.GetIncludeIn returned error: %v", err)
+	}
+
+	want := &gerrit.IncludedInInfo{
+		Branches: []string{
+			"master",
+		},
+		Tags: []string{
+			"1.1.0",
+		},
+	}
+
+	if !reflect.DeepEqual(includedInInfo, want) {
+		t.Errorf("Projects.GetIncludeIn returned %+v, want %+v", includedInInfo, want)
+	}
+}
